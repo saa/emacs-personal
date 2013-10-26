c8e0aebb23f10e0c4b3378f89d1e410cfc20b436
Sergey Abramyan <sa.abramyan@gmail.com>
2013-10-26 18:20:51 +0400

    Add textmate funs.

--- saa.el
+++ saa.el
@@ -67,6 +67,18 @@
   (interactive)
   (text-scale-increase 0))
 
+(defun textmate-shift-right (&optional arg)
+  (interactive)
+  (let ((deactivate-mark nil)
+        (beg (or (and mark-active (region-beginning))
+                 (line-beginning-position)))
+        (end (or (and mark-active (region-end)) (line-end-position))))
+    (indent-rigidly beg end (* (or arg 1) tab-width))))
+
+(defun textmate-shift-left (&optional arg)
+  (interactive)
+  (textmate-shift-right (* -1 (or arg 1))))
+
 ;; Bindings
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "C-c '") 'comment-dwim)
