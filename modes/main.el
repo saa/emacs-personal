991c971504a34dafcf7fe865b298f29939aec60d
Sergey Abramyan <sa.abramyan@gmail.com>
2014-03-17 01:19:56 +0400

    Changes

--- modes/main.el
+++ modes/main.el
@@ -7,8 +7,6 @@
 (setq prelude-flyspell nil)
 (global-flycheck-mode -1)
 
-;; (disable-theme 'zenburn)
-;; (load-theme 'solarized-dark t)
 (set-frame-font "Menlo-14")
 (set-fontset-font "fontset-default" 'cyrillic '("menlo" . "ISO10646-1"))
 (setq inhibit-startup-screen t
@@ -17,4 +15,3 @@
 (custom-set-variables '(shell-file-name "zsh"))
 (ido-vertical-mode)
 (setq-default tab-width 4)
-(electric-indent-mode +1)
--- saa.el
+++ saa.el
@@ -1,7 +1,7 @@
 (defconst user-init-dir
   (cond ((boundp 'user-emacs-directory)
          user-emacs-directory)
-        ((boundp 'user-init-directory)
+        ((boundp 'user-init--directory)
          user-init-directory)
         (t "~/.emacs.d/")))
 
@@ -11,9 +11,22 @@
   (load-file (expand-file-name file user-init-dir)))
 
 (load-user-file "personal/modes/main.el")
-(load-user-file "personal/modes/hooks.el")
 (load-user-file "personal/modes/adv.el")
-(load-user-file "personal/modes/erlang.el")
 (load-user-file "personal/modes/ocaml.el")
 (load-user-file "personal/modes/functions.el")
 (load-user-file "personal/modes/bindings.el")
+
+;; Hooks
+(remove-hook 'mouse-leave-buffer-hook #'prelude-auto-save-command)
+(add-hook 'text-mode-hook 'turn-off-flyspell t)
+(add-hook 'prog-mode-hook 'turn-off-flyspell t)
+(add-hook 'prog-mode-hook 'auto-complete-mode)
+(add-hook 'prog-mode-hook 'undo-tree-mode)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(add-hook 'prog-mode-hook 'whitespace-mode)
+
+;; Erlang
+(add-hook 'erlang-mode-hook (load-user-file "personal/modes/erlang.el"))
+(add-hook 'erlang-mode-hook (lambda () (autopair-mode)))
+(add-hook 'erlang-mode-hook
+          (lambda () (local-set-key (kbd "RET") 'newline-and-indent)))
