486c3991b0b6845f9ef2c92484d19505293c12d7
Sergey Abramyan <sa.abramyan@gmail.com>
2014-07-18 13:08:31 +0400

    All in one file

--- saa.el
+++ saa.el
@@ -1,18 +1,91 @@
-(defconst user-init-dir
-  (cond ((boundp 'user-emacs-directory)
-         user-emacs-directory)
-        ((boundp 'user-init--directory)
-         user-init-directory)
-        (t "~/.emacs.d/")))
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
 
-(defun load-user-file (file)
-  (interactive "f")
-  "Load a file in current user's configuration directory"
-  (load-file (expand-file-name file user-init-dir)))
+(flyspell-mode-off)
+(setq prelude-guru nil)
+(setq prelude-flyspell nil)
+(global-flycheck-mode -1)
 
-(load-user-file "personal/modes/main.el")
-(load-user-file "personal/modes/functions.el")
-(load-user-file "personal/modes/ocaml.el")
+(disable-theme 'zenburn)
+(load-theme 'base16-chalk t)
+(set-frame-font "Menlo-14")
+(set-fontset-font "fontset-default" 'cyrillic '("menlo" . "ISO10646-1"))
+(setq inhibit-startup-screen t
+      initial-scratch-message nil)
+(fset 'yes-or-no-p 'y-or-n-p)
+(custom-set-variables '(shell-file-name "zsh"))
+(ido-vertical-mode)
+(setq-default tab-width 4)
+
+(require 'auto-complete)
+(setq ac-quick-help-delay 0.5)
+(define-key ac-mode-map  [(alt tab)] 'auto-complete)
+
+(require 'smex)
+(smex-initialize)
+
+(require 'exec-path-from-shell)
+(exec-path-from-shell-initialize)
+
+(require 'ace-jump-mode)
+(require 'diminish)
+
+(require 'windmove)
+(windmove-default-keybindings)
+(setq windmove-wrap-around t)
+
+(require 'midnight)
+
+(require 'golden-ratio)
+(golden-ratio-mode)
+(diminish 'golden-ratio-mode)
+
+(require 'undo-tree)
+(global-undo-tree-mode 1)
+(diminish 'undo-tree-mode)
+(defalias 'redo 'undo-tree-redo)
+
+(require 'paren)
+(show-paren-mode +1)
+(setq show-paren-style 'parenthesis)
+(set-face-background 'show-paren-match-face "#6f6f6f")
+(set-face-foreground 'show-paren-match-face "#94bff3")
+
+(require 'whitespace)
+(setq
+ whitespace-line-column 120
+ whitespace-style '(face
+                    trailing
+                    lines
+                    lines-tail
+                    tabs
+                    space-before-tab space-after-tab
+                    indentation))
+
+(require 'protobuf-mode)
+
+;; Functions
+(defun text-scale-normal-size ()
+  (interactive)
+  (text-scale-increase 0))
+
+(defun textmate-shift-right (&optional arg)
+  (interactive)
+  (let ((deactivate-mark nil)
+        (beg (or (and mark-active (region-beginning))
+                 (line-beginning-position)))
+        (end (or (and mark-active (region-end)) (line-end-position))))
+    (indent-rigidly beg end (* (or arg 1) tab-width))))
+
+(defun textmate-shift-left (&optional arg)
+  (interactive)
+  (textmate-shift-right (* -1 (or arg 1))))
+
+(defun font-lock-comment-annotations ()
+  (font-lock-add-keywords
+   nil '(("\\<\\(FIX\\(ME\\)?\\|TODO\\|OPTIMIZE\\|HACK\\|REFACTOR\\):"
+          1 font-lock-warning-face t))))
 
 ;; Hooks
 (remove-hook 'mouse-leave-buffer-hook #'prelude-auto-save-command)
@@ -25,18 +98,36 @@
 
 ;; Bindings
 (global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "C-c '") 'comment-dwim)
 (global-set-key (kbd "C-c k") 'kill-region)
 (global-set-key (kbd "s-<up>") 'move-text-up)
 (global-set-key (kbd "s-<down>") 'move-text-down)
+(global-set-key (kbd "s-f") 'projectile-find-file)
 (global-set-key (kbd "C-0") 'text-scale-normal-size)
 (global-set-key (kbd "C-.") 'textmate-shift-right)
 (global-set-key (kbd "C-,") 'textmate-shift-left)
 (global-set-key (kbd "RET") 'newline-and-indent)
+;; Magit
+(eval-after-load 'prelude-mode
+  '(define-key prelude-mode-map (kbd "s-m co") 'magit-checkout))
+(eval-after-load 'prelude-mode
+  '(define-key prelude-mode-map (kbd "s-m pl") 'magit-pull))
+(eval-after-load 'prelude-mode
+  '(define-key prelude-mode-map (kbd "s-'") 'comment-dwim))
 
 ;; Erlang
-(add-hook 'erlang-mode-hook (load-user-file "personal/modes/erlang.el"))
 (add-hook 'erlang-mode-hook (lambda () (autopair-mode)))
 (add-hook 'erlang-mode-hook
           (lambda () (local-set-key (kbd "RET") 'newline-and-indent)))
+
 (add-hook 'erlang-mode-hook 'font-lock-comment-annotations)
+(setq erlang-root-dir "/usr/local/lib/erlang")
+(setq exec-path (cons "/usr/local/lib/erlang/bin" exec-path))
+(require 'erlang-start)
+
+(setq erlang-indent-level 4)
+(setq erlang-argument-indent 4)
+(setq erlang-indent-guard 4)
+
+(add-to-list 'ac-modes 'erlang-mode)
+(add-to-list 'load-path "~/.emacs.d/vendor/edts")
+(require 'edts-start)
